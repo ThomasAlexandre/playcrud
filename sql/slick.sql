5bd7f8b34948ac989a4ab84e1ff61c632de4c748
Thomas Alexandre <thomas.alexandre@devcode.se>
2012-11-28 18:06:31 +0100

    Sample database

---
+++ sql/slick.sql
@@ -0,0 +1,84 @@
+-- MySQL dump 10.13  Distrib 5.5.15, for osx10.6 (i386)
+--
+-- Host: localhost    Database: slick
+-- ------------------------------------------------------
+-- Server version	5.5.15-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `COFFEES`
+--
+
+DROP TABLE IF EXISTS `COFFEES`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `COFFEES` (
+  `COF_NAME` varchar(254) NOT NULL,
+  `SUP_ID` int(11) NOT NULL,
+  `PRICE` double NOT NULL,
+  `SALES` int(11) NOT NULL,
+  `TOTAL` int(11) NOT NULL,
+  PRIMARY KEY (`COF_NAME`),
+  KEY `SUP_FK` (`SUP_ID`),
+  CONSTRAINT `SUP_FK` FOREIGN KEY (`SUP_ID`) REFERENCES `SUPPLIERS` (`SUP_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `COFFEES`
+--
+
+LOCK TABLES `COFFEES` WRITE;
+/*!40000 ALTER TABLE `COFFEES` DISABLE KEYS */;
+INSERT INTO `COFFEES` VALUES ('Colombian',101,7.99,0,0),('Colombian_Decaf',101,8.99,0,0),('Espresso',150,9.99,0,0),('French_Roast',49,8.99,0,0),('French_Roast_Decaf',49,9.99,0,0);
+/*!40000 ALTER TABLE `COFFEES` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `SUPPLIERS`
+--
+
+DROP TABLE IF EXISTS `SUPPLIERS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `SUPPLIERS` (
+  `SUP_ID` int(11) NOT NULL,
+  `SUP_NAME` varchar(254) NOT NULL,
+  `STREET` varchar(254) NOT NULL,
+  `CITY` varchar(254) NOT NULL,
+  `STATE` varchar(254) NOT NULL,
+  `ZIP` varchar(254) NOT NULL,
+  PRIMARY KEY (`SUP_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `SUPPLIERS`
+--
+
+LOCK TABLES `SUPPLIERS` WRITE;
+/*!40000 ALTER TABLE `SUPPLIERS` DISABLE KEYS */;
+INSERT INTO `SUPPLIERS` VALUES (49,'Superior Coffee','1 Party Place','Mendocino','CA','95460'),(101,'Acme, Inc.','99 Market Street','Groundsville','CA','95199'),(150,'The High Ground','100 Coffee Lane','Meadows','CA','93966');
+/*!40000 ALTER TABLE `SUPPLIERS` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-11-28 18:05:01
