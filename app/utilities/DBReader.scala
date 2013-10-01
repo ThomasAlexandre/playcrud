9b932c6ddd29e5ddb7955d941d1cb4116bfcfc6d
unknown <thomas_alexandre@yahoo.com>
2013-10-01 15:40:26 +0200

    Go from Build.scala back to a build.sbt and update dependencies

--- app/utilities/DBReader.scala
+++ app/utilities/DBReader.scala
@@ -36,10 +36,10 @@ case class TableProperty(
 class DBReader(val config: Config) extends Logged {
 
   // Load the driver
-  val driver = classOf[com.mysql.jdbc.Driver]
-  val h2Driver = classOf[org.h2.Driver]
-  val postgreSQLDriver = classOf[org.postgresql.Driver]
-
+  val postgresqldriver = classOf[org.postgresql.Driver].newInstance()
+  val mySqldriver = classOf[com.mysql.jdbc.Driver].newInstance()
+  val h2Driver = classOf[org.h2.Driver].newInstance()
+  
   // Setup the connection
   val conn = DriverManager.getConnection(config.dburl, config.username, config.password)
   val metadata = conn.getMetaData()
